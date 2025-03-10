403412a220b4428956ba8ef9460c104aea9295ed
Unathi Mokgoetsi <u23552311@up.ac.za>
2025-03-10 21:50:52 +0200

    Add Person.cs model

---
+++ u23552311_INF272._Prac2/Models/Person.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace u23552311_INF272._Prac2.Models
+{
+	public class Person
+	{
+
+        [Display(Name = "Student number")]
+
+        public string StudentNumber { get; set; }
+
+        [Display(Name = "Fist Name")]
+
+        public string FirstName { get; set; }
+
+
+        [Display(Name = "Last Name")]
+
+        public string LastName { get; set; }
+
+        [Display(Name = "Email Address")]
+
+        public string Email { get; set; }
+
+        [Display(Name = "Link to personal page")]
+
+        public string myLink { get; set; }
+    }
+}
